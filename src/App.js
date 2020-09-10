2d6e2b6eff5602e1c809db31d1cfe8efc07ecdf8
izupu02 <yeoboseyo_min@icloud.com>
2020-09-10 19:55:18 +0900

    useReducer で作成 #3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
+
+const ENTRIES = {
+    IN: 1,
+    DE: 2
+}
+
+const initialCount = 0
+
+const reducer = (state, action) => {
+  if (action.type === ENTRIES.IN) {
+    return state + 1;
+  }
+  else if(action.type === ENTRIES.DE) {
+    return state - 1;
+  }
+  return state;
+}
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const increment = () => { setCount(count + 1) };
-  const decrement = () => { setCount(count - 1) };
+  const [count, dispatch] = useReducer(reducer, initialCount);
 
   return (
     <div>
       count:{count}
-      <button onClick={() => {increment()}}>+</button>
-      <button onClick={() => {decrement()}}>-</button>
+      <button onClick={() => dispatch({ type:ENTRIES.IN })}>+</button>
+      <button onClick={() => dispatch({ type: ENTRIES.DE })}>-</button>
     </div>
   );
 }
