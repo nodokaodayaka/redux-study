7d97c2665d930dccaa55c0fca570838d1c27c2ec
nodokaodayaka <wankosob@gmail.com>
2020-10-07 20:26:30 +0900

    front to backend connect

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([{text:'first'}]);
+
+  const clickHandler = () => {
+    const work = axios.get('http://localhost:4000/').then(res => {
+      setData(res.data);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={clickHandler}>Click Me!</button>
+      <div>{data[0].text}</div>
     </div>
   );
 }
